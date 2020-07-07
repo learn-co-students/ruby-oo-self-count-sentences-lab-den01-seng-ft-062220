71e05c8fbe24a3e561eb7fc083e3e746dc32aed4
Gerald Bivens <geraldbivens@gmail.com>
2020-07-07 06:08:36 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,25 @@
 require 'pry'
 
 class String
+  # How do these instance methods know what self is referencing?
+  # Is it simply how the tests are written in the spec file? 
+  # The tests are passing the method the string.
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
+    # square brackets vs curly braces vs parentheses?
+    # What is the plus sign doing here?
   end
 end
